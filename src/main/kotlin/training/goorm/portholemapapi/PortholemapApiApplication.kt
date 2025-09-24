c868a88a9e49676c9b4b4530cd0f8d0013a5f8cf
junhaesung <seong0428@gmail.com>
2025-09-24 15:48:44 +0900

    Initial commit

---
+++ src/main/kotlin/training/goorm/portholemapapi/PortholemapApiApplication.kt
@@ -0,0 +1,11 @@
+package training.goorm.portholemapapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortholemapApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<PortholemapApiApplication>(*args)
+}
---
+++ src/test/kotlin/training/goorm/portholemapapi/PortholemapApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package training.goorm.portholemapapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortholemapApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
