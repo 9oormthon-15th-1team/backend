4bee565eab67bd7c8e81cdb813f7f67ecbe48f96
junhaesung <seong0428@gmail.com>
2025-09-25 15:30:28 +0900

    feat: Refactor cors config

--- src/main/kotlin/training/goorm/portholemapapi/controller/PotholeController.kt
+++ src/main/kotlin/training/goorm/portholemapapi/controller/PotholeController.kt
@@ -1,8 +1,6 @@
 package training.goorm.portholemapapi.controller
 
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.responses.ApiResponse as SwaggerApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 import training.goorm.portholemapapi.dto.ApiResponse
